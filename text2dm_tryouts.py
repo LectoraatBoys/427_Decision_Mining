42e06903303062ab3bcea1d0409a03e5fa2b4fe6
pebro18 <pebro06@gmail.com>
2023-05-17 14:24:13 +0200

    Made activation script to implement activation for frontend
    Co-authored-by: SiebevanZwaal <SiebevanZwaal@users.noreply.github.com>

--- text2dm_tryouts.py
+++ text2dm_tryouts.py
@@ -51,7 +51,18 @@ his verbal, math and abstract levels."""
 
 # XML EXTRACTION
 def xml():
-    input = 'Playing tennis depends on the weather which is influenced by the season. The season follows from the date.'
+    input = """1 The competent authority of an institution shall be obliged to provide students of higher education who are enrolled in a study programme for the teaching profession covered by the Higher Education and Scientific Research Act, or who are otherwise studying for a certificate of sufficient pedagogical competence, and who are training for a position in education, with the opportunity to obtain the experience required as part of their study programme at the institution.
+
+    2 The obligation referred to in the first paragraph comprises 5% of the total vocational education and training to be provided by the institution in the relevant academic year. The competent authority may set a higher percentage provided this is in accordance with the proper course of action within the institution.
+
+    3 The competent authority may deny a ho-student further access to the institution if the student acts contrary to the foundation and objectives of the institution. A decision to deny access to the institution will be communicated by sending or issuing a copy to the competent authority of the educational institution concerned or to the relevant state examination board, and to the inspectorate. If the governing body of a special school refuses to admit a higher education student, it will announce this decision, in writing and with reasons, by sending or issuing a copy to the higher education student, without prejudice to the provisions in the previous sentence.
+
+    4 The competent authority of the institution shall arrange the activities related to supervision by the teachers of the higher education students in the institution in agreement with the teachers, as well as in agreement with the educational institutions involved, or, if it concerns higher education students who are preparing to take a state examination in order to obtain a certificate of competence or a certificate of adequate pedagogical and didactic preparation, in agreement with the state examination committee involved.
+
+    5 Our Minister may grant the competent authority full or partial exemption from the obligation referred to in the first paragraph based on special circumstances. The exemption applies for one academic year.
+
+    6 The institutions to which higher professional education students as referred to in the first paragraph are admitted, shall be accessible to the inspectorate charged with the supervision of the educational institutions, to the members of the management and the teachers of these educational institutions to be appointed by them, as well as to the members of the State Examination Boards involved, insofar as this is necessary for the exercise of supervision of the practical training or supervision of the practical training of the higher professional education students present at the institution.
+    """
     xmlstr = text2dm.get_xml(input)
     #print(xmlstr)
     with open("results tryouts/xml_DRD.dmn", "w") as f:
@@ -75,7 +86,18 @@ def drd_multiple():
 
 # LOGIC TABLE EXTRACTION
 def logic_table():
-    input_logic = 'Your discount is determined from age. If your age is lower than 20, you get 10% discount. Otherwise the discount is 5%.'
+    input_logic = """1 The competent authority of an institution shall be obliged to provide students of higher education who are enrolled in a study programme for the teaching profession covered by the Higher Education and Scientific Research Act, or who are otherwise studying for a certificate of sufficient pedagogical competence, and who are training for a position in education, with the opportunity to obtain the experience required as part of their study programme at the institution.
+
+    2 The obligation referred to in the first paragraph comprises 5% of the total vocational education and training to be provided by the institution in the relevant academic year. The competent authority may set a higher percentage provided this is in accordance with the proper course of action within the institution.
+
+    3 The competent authority may deny a ho-student further access to the institution if the student acts contrary to the foundation and objectives of the institution. A decision to deny access to the institution will be communicated by sending or issuing a copy to the competent authority of the educational institution concerned or to the relevant state examination board, and to the inspectorate. If the governing body of a special school refuses to admit a higher education student, it will announce this decision, in writing and with reasons, by sending or issuing a copy to the higher education student, without prejudice to the provisions in the previous sentence.
+
+    4 The competent authority of the institution shall arrange the activities related to supervision by the teachers of the higher education students in the institution in agreement with the teachers, as well as in agreement with the educational institutions involved, or, if it concerns higher education students who are preparing to take a state examination in order to obtain a certificate of competence or a certificate of adequate pedagogical and didactic preparation, in agreement with the state examination committee involved.
+
+    5 Our Minister may grant the competent authority full or partial exemption from the obligation referred to in the first paragraph based on special circumstances. The exemption applies for one academic year.
+
+    6 The institutions to which higher professional education students as referred to in the first paragraph are admitted, shall be accessible to the inspectorate charged with the supervision of the educational institutions, to the members of the management and the teachers of these educational institutions to be appointed by them, as well as to the members of the State Examination Boards involved, insofar as this is necessary for the exercise of supervision of the practical training or supervision of the practical training of the higher professional education students present at the institution.
+    """
     df_result_logic = text2dm.extract_logic_table(input_logic)
     print('LOGIC TABLE EXTRACTION')
     print(df_result_logic)
@@ -87,4 +109,4 @@ def logic_table():
 if __name__ == "__main__":
     # choose here what you want to test
     # drd_graph()
-    tuple_single_example()
+    logic_table()
